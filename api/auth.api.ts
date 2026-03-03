cc18ff2dfdfab01183b201317564775accb1b05b
anubus298 <araristaf@gmail.com>
2026-03-03 13:20:33 +0000

    fix: add auth.api

---
+++ api/auth.api.ts
@@ -0,0 +1,19 @@
+export const login = async (
+  username: string,
+  password: string,
+): Promise<string> => {
+  const res = await fetch(
+    "192.168.1.111:8075/sdkboard/api/users/authentification.php",
+    {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+        login_token: "SDKWOOD",
+        code_token: "SDKWOOD/2026@!!",
+      },
+    },
+  );
+  const data = await res.json();
+  return data.title;
+};
